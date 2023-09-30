c59f6c4b66794fa2dc58835a82d7432aa927e141
Rayyan <1875920765lennart@gmail.com>
2023-09-30 19:46:23 +0800

    getmap /hello

--- src/main/java/com/reviewlah/controller/BookController.java
+++ src/main/java/com/reviewlah/controller/BookController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BookController {
 
+    @GetMapping("/hello")
+    public  String hello(){
+        return "This is ReviewLah! -G16";
+    }
     @GetMapping("/book/{id}")
     public String getById(@PathVariable Integer id){
         System.out.println("id == >" + id);
