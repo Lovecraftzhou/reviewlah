35a3f9da4e69238e47a6f0e71900b8621afc96a6
Lovecraftzhou <894022@qq.com>
2023-09-30 18:49:51 +0800

    Project init

---
+++ src/main/java/com/reviewlah/ReviewlahApplication.java
@@ -0,0 +1,13 @@
+package com.reviewlah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewlahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewlahApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/reviewlah/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.reviewlah.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BookController {
+
+    @GetMapping("/book/{id}")
+    public String getById(@PathVariable Integer id){
+        System.out.println("id == >" + id);
+        return "id = " + id;
+    }
+}
