b367edc348fbce8cb534cd45b472e9a1ef1f06b2
Rayyan <1875920765lennart@gmail.com>
2023-10-02 22:49:39 +0800

    vue config

--- frontend-vue/vue.config.js
+++ frontend-vue/vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+//spring boot run on 8080, proxying requests with the '/api' to a Spring Boot application running on 8080
 // vue.config.js
 module.exports = {
   // https://cli.vuejs.org/config/#devserver-proxy
