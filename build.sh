9a0f44dd6bcd734995113a597509b670868d8eaf
Lovecraftzhou <894022@qq.com>
2023-11-03 01:06:37 +0800

    eureka config update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC2164
 docker stop reviewlah-jenkins reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
-docker rm reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
+#docker rm reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
 cd /home/jenkins_home/workspace/reviewlah-mic
 docker compose up -d --build
 docker start reviewlah-jenkins
