bdc3a8327f021bfa7c85d747398972d7b9f513bc
Lovecraftzhou <894022@qq.com>
2023-11-02 21:47:21 +0800

    Config file update

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # shellcheck disable=SC2164
-docker stop jenkins reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
+docker stop reviewlah-jenkins reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
 docker rm reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
-cd /var/jenkins_home/workspace/reviewlah-mic
+cd /home/jenkins_home/workspace/reviewlah-mic
 docker compose up -d --build
-docker start jenkins
+docker start reviewlah-jenkins
