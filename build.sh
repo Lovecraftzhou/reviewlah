479c4c6011b113146c52d301cf7180588cd990ec
Lovecraftzhou <894022@qq.com>
2023-11-03 16:57:22 +0800

    URL update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck disable=SC2164
-docker stop reviewlah-back reviewlah-vue reviewlah-jenkins reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
+docker stop reviewlah-back reviewlah-vue reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
 #docker rm reviewlah-admin reviewlah-admin-vue reviewlah-comment reviewlah-customer reviewlah-eureka reviewlah-gateway reviewlah-merchant reviewlah-message
 cd /home/jenkins_home/workspace/reviewlah-mic
 docker compose up -d --build
